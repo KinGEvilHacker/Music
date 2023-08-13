e3c2ec13b80644b989f49fcf53e481a2d803cc8b
KinGEvilHacker <103206714+KinGEvilHacker@users.noreply.github.com>
2023-08-13 15:09:24 +0530

    Update strings.py

--- Music/helpers/strings.py
+++ Music/helpers/strings.py
@@ -63,7 +63,7 @@ class TEXTS:
     SOURCE = (
         "**Source 📦:**\n\n"
         "**Note:** \n__» The source code is available on GitHub. You can find the link below.__\n"
-        "__» Every project available under The-HellBot are open-source and free to use and modify to your needs.__\n"
+        "__» Every project available under ˹ᴍɪss ✗ network are open-source and free to use and modify to your needs.__\n"
         "__» Anyone pretending to be the developer of this bot and selling the code, is a scammer.__\n\n"
         "__» Please consider giving a star to the repository if you liked the project.__\n"
         "__» Feel free to contact us if you need any help regarding the source code.__\n\n"
